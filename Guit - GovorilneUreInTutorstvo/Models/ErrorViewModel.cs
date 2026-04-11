a37e5f171dfe9410395bf01744251ff9da60d950
Kristan <117606644+Kr1stan56@users.noreply.github.com>
2026-04-11 14:33:32 +0200

    l

--- Guit - GovorilneUreInTutorstvo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Guit___GovorilneUreInTutorstvo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
